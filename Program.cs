8df5fc728bcd96274e22e085de2eae7cb584bd71
Cleyton Jesus <cleytonjesus007@gmail.com>
2024-04-19 00:09:56 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nk = new("1234-5678nk","NK-2012","asder4321ki33",40);
+Iphone iph = new("4321-8765iph","IPH-2024","iph-2024tyui903302",60);
+
+nk.Ligar();
+iph.Ligar();
+nk.InstalarAplicativo("Bope esponja");
+iph.InstalarAplicativo("Bope esponja 2");
